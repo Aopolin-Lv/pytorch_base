5c6dae3c6488543968ceeedb684911d0ac8aca24
aopolin <aopolin.ii@gmail.com>
2020-11-06 22:12:30 +0800

    Update: fix bug 1. batch sort 2. using pak_paded to resolve problem about bilstm padding 3. finish train() function

--- test.py
+++ test.py
@@ -25,13 +25,15 @@ class Config(object):
     status = 'train'  # 执行 train_eval or test, 默认执行train_eval
     output_folder = 'output_data/'  # 已处理的数据所在文件夹
     data_path = 'data/'  # 数据集所在路径
-    emb_file = 'data/glove.6B.300d.txt'  # 预训练词向量所在路径
-    pretrain_emb_file_suffix = "_pretrain_embed.pth"
+    embed_file = 'data/glove.6B.300d.txt'  # 预训练词向量所在路径
     vocab_file_suffix = "_vocab.json"
+    PRETRAINED_VOCAB_FILENAME = "pretrained" + vocab_file_suffix
+    pretrain_emb_file_suffix = "_pretrain_embed.pth"
     emb_format = 'glove'  # embedding format: word2vec/glove
     min_word_freq = 1  # 最小词频
     max_len = 40  # 采样最大长度
     MAX_VOCAB_SIZE = 30000
+    USE_EMBED_VOCAB = True
 
     # 训练参数
     epochs = 120  # epoch数目，除非early stopping, 先开20个epoch不微调,再开多点epoch微调
@@ -50,7 +52,7 @@ class Config(object):
     model_dropout = 0.5  # dropout
     fc_dropout = 0.5  # dropout
     num_layers = 2  # LSTM层数
-    embed_dim = 128  # 未使用预训练词向量的默认值
+    embed_dim = 300  # 未使用预训练词向量的默认值
     use_embed = True  # 是否使用预训练
     use_gru = True  # 是否使用GRU
     grad_clip = 4.  # 梯度裁剪阈值
@@ -77,7 +79,7 @@ def Replace_Abbreviations(sentence):
     """
     替换缩写字符并且进行大写->小写
     :param sentence:  待清洗语料
-    :return: cleaned_sentence: 清洗完成后的语料
+    :return cleaned_sentence: 清洗完成后的语料
     """
     cleaned_sentence = sentence
     cleaned_sentence = Rule.pat_letter.sub(' ', cleaned_sentence).strip().lower()
@@ -97,7 +99,7 @@ def Load_Data(input_file_name: str):
     """
     从文件读入数据
     :param input_file_name: 数据文件名称
-    :return: sentence: 读取的数据
+    :return sentence: 读取的数据
     """
     sentence = []
     read_type_list = ["train", "dev", "test"]
@@ -131,10 +133,11 @@ def Create_Wordlist(text: list, max_vocab_size: int):
     创建词表，统计词频，词数，标签数
     :param text: 句子列表
     :param max_vocab_size:
-    :return: word_frequency: 词表, label_count: 标签数
+    :return word_frequency: 词表
+    :return label_count:  标签数
     """
     vocab_dict = dict(Counter(text).most_common(max_vocab_size - 1))
-    vocab_dict["<unk>"] = len(text) - np.sum(list(vocab.values()))
+    vocab_dict["<unk>"] = len(text) - np.sum(list(vocab_dict.values()))
     return vocab_dict
 
 
@@ -156,7 +159,7 @@ def Build_Dict(text, max_vocab_size=50000):
     for index, w in enumerate(ls):
         vocab_dict[w[0]] = index + 2
 
-    Save_Vocab(vocab_dict, opt.output_folder, opt.data_name)
+    Save_Vocab(vocab_dict, os.path.join(opt.output_folder, opt.data_name))
     print("Save vocab success")
     return vocab_dict, total_words
 
@@ -209,7 +212,7 @@ def ConvertWord2Idx(sentences_label_list, vocab):
     return output
 
 
-def Build_Batch(data_list, batch_size, sorted_by_len=True, shuffle=True):
+def Build_Batch(dataset, batch_size, sorted_by_len=True, shuffle=True):
     def Get_Minibatches(data_len, minibatch_size, shuffle=True):
         # 得到按照batch大小划分的index_list,如batch_size == 2，idx_list = [0, 2, 4, 6...]
         idx_list = np.arange(0, data_len, batch_size)
@@ -231,26 +234,44 @@ def Build_Batch(data_list, batch_size, sorted_by_len=True, shuffle=True):
 
         # 用0矩阵初始化x(numpy matrix)
         x = np.zeros((n_samples, max_len)).astype("int32")
-        # 将一个batch里每一个sentence的标签st -> numpy matrix
+        # 将一个batch里每一个sentence的标签label -> numpy matrix
         x_labels = np.array(labels).astype("int32")
+        x_lengths = np.array(lengths).astype("int32")
 
         for idx, sen in enumerate(sentences):
             x[idx, :lengths[idx]] = sen[0]
-        return x, x_labels
+            # x[idx, lengths[idx]:] = 1.                               # 做padding
+        return x, x_labels, x_lengths
 
     def Generate_Batch(data_list, batch_size, shuffle):
+        """
+        dataset: [len(sentences), [sentence, label]]
+        """
+        # 拿到有序/随机的minibatch下标组成的minibatches列表
         minibatches = Get_Minibatches(len(data_list), batch_size, shuffle)
         result = []
         for minibatch in minibatches:
+            # 通过index在原先的dataset中找到相应batch的数据组成的列表[batch_size, [sentence, label]]
             mb_sent = [data_list[t] for t in minibatch]
-            mb_x, mb_x_labels = Fill_Data(mb_sent)
-            result.append((mb_x, mb_x_labels))
+            # 填充数据
+            mb_x, mb_x_labels, mb_x_lengths = Fill_Data(mb_sent)
+            result.append((mb_x, mb_x_labels, mb_x_lengths))
         return result
 
-    if sorted_by_len:
-        data_list = sorted(data_list, key=lambda k: len(k[0][0]))
+    def Sort_Dataset(dataset):
+        """
+        按照降序对dataset进行排序
+        :param dataset:
+        :return out_dataset:
+        """
+        sorted_idx = sorted(range(len(dataset)), key=lambda i: len(dataset[i][0]))
+        out_dataset = [dataset[idx] for idx in sorted_idx]
+        return out_dataset
 
-    data_batch = Generate_Batch(data_list, batch_size, shuffle)
+    # if sorted_by_len:
+    dataset = Sort_Dataset(dataset)
+
+    data_batch = Generate_Batch(dataset, batch_size, shuffle)
     return data_batch
 
 
@@ -305,19 +326,23 @@ def Load_Embeddings(emb_file, vocab):
     return embeddings, emb_dim
 
 
-def Save_Vocab(vocab, output_folder, data_name):
+def Save_Vocab(vocab, filename):
     """
     保存由训练集构建的词表
-    :param vocab:
-    :param output_folder:
-    :param data_name:
+    :param vocab: 词表
+    :param filename: 输出文件目录
     :return:
     """
-    with open(os.path.join(output_folder, data_name + opt.vocab_file_suffix), "w") as j:
+    with open(filename, "w") as j:
         json.dump(vocab, j)
 
 
 def Load_Vocab(vocab_filename):
+    """
+    加载词表
+    :param vocab_filename: 词表的文件名称
+    :return vocab: 词表
+    """
     with open(vocab_filename, "r") as j:
         vocab = json.load(j)
     return vocab
@@ -330,7 +355,7 @@ def Save_Pth(pretrain_embed, embed_dim, output_folder, data_name):
     :param embed_dim: 预训练向量维度
     :param output_folder: 输出目录
     :param data_name: 文件名称
-    :return : None
+    :return:
     """
     embed = dict()
     embed["pretrain_embed"] = pretrain_embed
@@ -339,38 +364,14 @@ def Save_Pth(pretrain_embed, embed_dim, output_folder, data_name):
 
 
 def Load_Pth(embed_file_name):
+    """
+    加载预训练词向量
+    :param embed_file_name: 预训练词向量的文件名称
+    :return pretrained_embed: 预训练好的词向量
+    :return embed_dim: 词向量的维度
+    """
     embed_file = torch.load(embed_file_name)
     return embed_file["pretrain_embed"], embed_file["embed_dim"]
-
-
-# 读取数据
-t = Load_Data(opt.data_name)
-
-# 获得长句
-text = ""
-for i, (word_list, label) in enumerate(t):
-    sentences = " ".join(word_list)
-    text += sentences
-
-# 创建词表
-vocab_filename = os.path.join(opt.output_folder, opt.data_name + opt.vocab_file_suffix)
-if not os.path.exists(vocab_filename):
-    vocab, _ = Build_Dict(Word_Tokenize(text), opt.MAX_VOCAB_SIZE)
-else:
-    vocab = Load_Vocab(vocab_filename)
-
-idx_to_word = Idx2Word(vocab)
-word_to_idx = Word2Idx(vocab)
-word_counts, word_freqs = Word_Counts_and_Frequency(vocab)
-
-pretrain_emb_filename = os.path.join(opt.output_folder, opt.data_name + opt.pretrain_emb_file_suffix)
-if not os.path.exists(pretrain_emb_filename):
-    pretrain_emb, emb_dim = Load_Embeddings(opt.emb_file, vocab)
-else:
-    pretrain_emb, emb_dim = Load_Pth(pretrain_emb_filename)
-
-sentences_list = ConvertWord2Idx(t, vocab)
-data_batch = Build_Batch(sentences_list, opt.batch_size, sorted_by_len=False)
 
 
 class Atten(nn.Module):
@@ -380,12 +381,14 @@ class Atten(nn.Module):
 
     def foward(self, x):
         """
-        x: [batch, sen_max_len, embed_dim]
+
+        :param x: [batch, seq_len, embed_dim]
+        :return alpha: [batch, 1, seq_len]
         """
-        m = torch.tanh(x)  # [batch, sen_max_len, hidden_size]
-        m = self.atten(m).squeeze(2)  # [batch, sen_max_len]
-        alpha = nn.functional.softmax(m, dim=1).unsqueeze()     # [batch, 1, sen_max_len]
-        return alpha
+        m = torch.tanh(x)                                       # [batch, seq_len, hidden_size]
+        m = self.atten(m).squeeze(2)                            # [batch, seq_len]
+        alpha = nn.functional.softmax(m, dim=1).unsqueeze(1)    # [batch, 1, seq_len]
+        return alpha                                            # [batch, 1, seq_len]
 
 
 class AttenBiLSTM(nn.Module):
@@ -398,16 +401,156 @@ class AttenBiLSTM(nn.Module):
         self.atten = Atten(hidden_size)
         self.fc = nn.Linear(hidden_size, class_num)
 
-    def forward(self, x):
+    def forward(self, x, x_lengths):
         """
-        x: [batch_size, max_len]
+
+        :param x: [batch_size, max_len]
+        :param x_lengths: [batch_size]
+        :return:
         """
-        x = self.embedding(x)       # [batch_size, sen_max_len, embed_dim]
-        h, _ = self.BiLSTM(x)       # [batch_size, sen_max_len, num_directions * hidden_size]
-        h = h[:, :, :self.hidden_size] + h[:, :, self.hidden_size:]  # [batch, sen_max_len, hidden_size]
-        alpha = self.atten(h)       # [batch, 1, sen_max_len]
-        r = alpha.bmm(h, alpha)     # [batch, 1, hidden_size]
-        r = r.squeeze(1)            # [batch, hidden_size]
-        h = torch.tanh(r)           # [batch, hidden_size]
-        logits = self.fc(h)         # [batch, class_num]
+        x = self.embedding(x)                                  # [batch_size, sen_max_len, embed_dim]
+        x = torch.nn.utils.rnn.pack_padded_sequence(x, x_lengths, batch_first=True, enforce_sorted=False)
+        h, _ = self.BiLSTM(x)                                  # [batch_size, sen_max_len, num_directions * hidden_size]
+        h1, _ = torch.nn.utils.rnn.pad_packed_sequence(h, batch_first=True)
+        # [batch_size, sen_max_len, 2, hidden_size]
+        h1 = h1.view((len(x_lengths), np.max(x_lengths), 2, self.hidden_size))
+        h_foward = h1[:, :, 0, :]                              # [batch_size, seq_len, hidden_size]
+        h_backward = h1[:, :, 1, :]                            # [batch_size, seq_len, hidden_size]
+        h1 = h_foward + h_backward                             # [batch_size, seq_len, hidden_size]
+
+        alpha = self.atten(h1)                                 # [batch_size, 1, sen_max_len]
+        r = alpha.bmm(h, alpha)                                # [batch_size, 1, hidden_size]
+        r = r.squeeze(1)                                       # [batch_size, hidden_size]
+        h = torch.tanh(r)                                      # [batch_size, hidden_size]
+        logits = self.fc(h)                                    # [batch_size, class_num]
         return logits
+
+
+def Build_Dict_From_Pretrain(pretrianed_filename):
+    """
+    用预训练的词向量构建词典
+    :param pretrianed_filename: 预训练词向量的文件名称
+    :return vocab_embed_dict: 词典
+    """
+    with open(pretrianed_filename, "r", encoding="utf-8") as f:
+        vocab_embed_dict = dict()
+        vocab_embed_dict["<unk>"] = 0
+        vocab_embed_dict["<pad>"] = 1
+        for i, line in enumerate(f):
+            vocab_embed_dict[line.split()[0]] = i + 2
+
+    Save_Vocab(vocab_embed_dict, os.path.join(opt.output_folder, opt.PRETRAINED_VOCAB_FILENAME))
+    print("Save vocab success")
+    return vocab_embed_dict
+
+
+class Evaluate(object):
+    def __init__(self):
+        self.val = 0.
+        self.avg = 0.
+        self.sum = 0.
+        self.count = 0.
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def accuracy(logits, targets):
+    """
+
+    :param logits: [batch_size, class_num]
+    :param targets: [batch_size]
+    :return:
+    """
+    corrects = (torch.max(logits, 1)[1].view(targets.size()).data == targets.data).sum()
+    return corrects.item() * (100.0 / targets.size(0))
+
+
+def train(data_batch, model, criterion, optimizer):
+    model.train()
+
+    losses = Evaluate()
+    accs = Evaluate()
+
+    for i, (sents, labels, lengths) in enumerate(data_batch):
+
+        sents = torch.from_numpy(sents).long()
+        labels = torch.from_numpy(labels).long()
+        # lengths = torch.from_numpy(lengths).long()
+
+        logits = model(sents, lengths)
+
+        # forward pass
+        loss = criterion(logits, labels)
+
+        # backward pass
+        optimizer.zero_grad()
+        loss.backward()
+
+        # update parameters
+        optimizer.step()
+
+        # calculate acc
+        accs.update(accuracy(logits, labels))
+        losses.update(loss.item())
+
+        # 打印状态
+        if i % 100 == 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Accuracy {acc.val:.3f} ({acc.avg:.3f})'.format(20, i, len(data_batch),
+                                                                          loss=losses,
+                                                                          acc=accs))
+
+
+def main():
+    # 读取数据
+    t = Load_Data(opt.data_name)
+
+    # 获得长句
+    text = ""
+    for i, (word_list, label) in enumerate(t):
+        sentences = " ".join(word_list)
+        text += sentences
+
+    # 创建词表
+    if opt.USE_EMBED_VOCAB:
+        vocab_filename = os.path.join(opt.output_folder, opt.PRETRAINED_VOCAB_FILENAME)
+    else:
+        vocab_filename = os.path.join(opt.output_folder, opt.data_name + opt.vocab_file_suffix)
+    if not os.path.exists(vocab_filename):
+        # vocab, _ = Build_Dict(Word_Tokenize(text), opt.MAX_VOCAB_SIZE)
+        vocab = Build_Dict_From_Pretrain(opt.embed_file)
+    else:
+        vocab = Load_Vocab(vocab_filename)
+
+    idx_to_word = Idx2Word(vocab)
+    word_to_idx = Word2Idx(vocab)
+    word_counts, word_freqs = Word_Counts_and_Frequency(vocab)
+
+    pretrain_emb_filename = os.path.join(opt.output_folder, opt.data_name + opt.pretrain_emb_file_suffix)
+    if not os.path.exists(pretrain_emb_filename):
+        pretrain_emb, emb_dim = Load_Embeddings(opt.embed_file, vocab)
+    else:
+        pretrain_emb, emb_dim = Load_Pth(pretrain_emb_filename)
+
+    sentences_list = ConvertWord2Idx(t, vocab)
+    data_batch = Build_Batch(sentences_list, opt.batch_size, sorted_by_len=False)
+    print("batch")
+
+    # model
+    model = AttenBiLSTM(vocab_size=len(vocab), embed_dim=opt.embed_dim, hidden_size=opt.embed_dim,
+                        class_num=opt.class_num, pretrain_embed=pretrain_emb, num_layers=opt.num_layers)
+
+    # loss function
+    criterion = nn.CrossEntropyLoss().to(opt.device)
+
+    # optimizer
+    optimizer = torch.optim.Adam(params=model.parameters(), lr=opt.lr)
+
+
+if __name__ == "__main__":
+    main()
